214ac4adbb4f18c78311489eb61037f39edaa778
Gökhan Akpınar <gkhnkpnr@icloud.com>
2022-06-17 14:04:14 +0300

    Consumer created.

---
+++ week-4-assigment-consumer/src/main/java/com/example/week4assigmentconsumer/Week4AssigmentConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.week4assigmentconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4AssigmentConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4AssigmentConsumerApplication.class, args);
+	}
+
+}
