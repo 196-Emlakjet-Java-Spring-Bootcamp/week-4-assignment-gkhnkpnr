c4aed9058a12220ec9e413e5e8c111a5cb07a38c
Gökhan Akpınar <gkhnkpnr@icloud.com>
2022-06-17 14:35:36 +0300

    UserService for Producer created.

---
+++ week-4-assigment-producer/src/main/java/com/example/week4assigmentproducer/business/UserService.java
@@ -0,0 +1,7 @@
+package com.example.week4assigmentproducer.business;
+
+import com.example.week4assigmentproducer.entity.User;
+
+public interface UserService {
+    void createUser(User user);
+}
---
+++ week-4-assigment-producer/src/main/java/com/example/week4assigmentproducer/business/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.week4assigmentproducer.business;
+
+import com.example.week4assigmentproducer.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void createUser(User user) {
+
+    }
+}
